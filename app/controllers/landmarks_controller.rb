fece56d75c5207dbb521474fdcbc76b3ce8cef86
Alex Zdatny <alexzdatny@gmail.com>
2018-06-06 00:21:34 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -12,12 +12,12 @@ get '/landmarks' do
 
  get '/landmarks/:id' do
    @landmark = Landmark.find(params[:id])
-   erb :"/landmarks/show"
+   erb :'/landmarks/show'
  end
 
  get '/landmarks/:id/edit' do
    @landmark = Landmark.find(params[:id])
-   erb :"/landmarks/edit"
+   erb :'/landmarks/edit'
  end
 
  post '/landmarks/:id' do
